f3b7f05756cc9736bbfbc71cbeac2d3312845875
rod-bryan <rodbryanpagsanjan@outlook.com>
2023-04-01 19:33:47 +0800

    git commit new

--- Seleniumjavatest/src/test/java/com/curademo/testcases/Curademotest.java
+++ Seleniumjavatest/src/test/java/com/curademo/testcases/Curademotest.java
@@ -1,40 +1,49 @@
 package com.curademo.testcases;
 
 import java.time.Duration;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
 public class Curademotest {
 	
-	@Test
-	public void testGoalSetter() {
+	WebDriver driver;
+	
+	@BeforeTest
+	public void setup() {
 		
-		//githubtest
-		//open browser
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
-        WebDriver driver = new ChromeDriver(options);
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
         
         //open url
         driver.get("https://katalon-demo-cura.herokuapp.com/");
-        
-        
         String Actualtitle = driver.getTitle();
         System.out.println(Actualtitle);
         String ExpectedTitle = "CURA Healthcare Service";
         Assert.assertEquals(ExpectedTitle, Actualtitle);
-        
-        //navigate homepage
-        //a[@id='menu-toggle']
+           
+	}
+	
+	
+	@Test(priority = 1)
+	public void testBookAppointmentOne() {
+		
+	
+      	//navigate homepage
         driver.findElement(By.xpath("//a[@id='menu-toggle']")).click();
         driver.findElement(By.xpath("//a[@href='profile.php#login']")).click();
         
+         
         //input username
         driver.findElement(By.xpath("//input[@id=\"txt-username\"]")).sendKeys("John Doe");
         
@@ -44,6 +53,25 @@ public class Curademotest {
         //click login
         driver.findElement(By.xpath("//button[@id='btn-login']")).click();
         
+        //Facility
+        //driver.findElement(By.xpath("//select[@id='combo_facility']")).click();
+        //driver.findElement(By.xpath("//option[@value='Hongkong CURA Healthcare Center']")).click();
+        
+        // Select Facility 
+        WebElement selectElementBryanSteph = driver.findElement(By.xpath("//select[@id='combo_facility']")); 
+        Select selectFacilty = new Select(selectElementBryanSteph); 
+        
+        selectFacilty.selectByValue("Tokyo CURA Healthcare Center"); 
+        
+        
+        //readmission
+        driver.findElement(By.xpath("//label[normalize-space()='Apply for hospital readmission']")).click();
+        
+        //healthcare program
+        driver.findElement(By.xpath("//label[normalize-space()='Medicare']")).click();
+       
+        
+        
         //input visitdate
         driver.findElement(By.xpath("//input[@id=\"txt_visit_date\"]")).sendKeys("31/03/2023");
         
@@ -52,19 +80,87 @@ public class Curademotest {
         
         //click Book Appointment 
         driver.findElement(By.xpath("//button[@id='btn-book-appointment']")).click();
+         
+        	
+	}
+	
+	
+	@Test(priority = 2)
+	public void testBookAppointmentTwo() {
+		
+	
+        //click Make Appointment
+        driver.findElement(By.xpath("//a[@id='btn-make-appointment']")).click();
+        
+        //Facility
+        //driver.findElement(By.xpath("//select[@id='combo_facility']")).click();
+        //driver.findElement(By.xpath("//option[@value='Hongkong CURA Healthcare Center']")).click();
+        
+        // Select Facility 
+        WebElement selectElementBryanSteph = driver.findElement(By.xpath("//select[@id='combo_facility']")); 
+        Select selectFacilty = new Select(selectElementBryanSteph); 
+                   
+        selectFacilty.selectByValue("Hongkong CURA Healthcare Center"); 
+     
+        
+        //readmission
+        driver.findElement(By.xpath("//label[normalize-space()='Apply for hospital readmission']")).click();
+        
+        //healthcare program
+        driver.findElement(By.xpath("//label[normalize-space()='Medicaid']")).click();
+      
         
         
-        //driver.findElement(By.xpath("//input[@id=\"email\"]")).sendKeys("123@email.com");
-        //driver.findElement(By.xpath("//input[@id=\"password\"]")).sendKeys("123");
-        //driver.findElement(By.xpath("//button[@type='submit']")).click();
+        //input visitdate
+        driver.findElement(By.xpath("//input[@id=\"txt_visit_date\"]")).sendKeys("25/08/2023");
+        
+        //input comment
+        driver.findElement(By.xpath("//textarea[@id=\"txt_comment\"]")).sendKeys("Sample comment only.");
+        
+        //click Book Appointment 
+        driver.findElement(By.xpath("//button[@id='btn-book-appointment']")).click();
+         	
+	}
+	
+	@Test(priority = 3)
+	public void testBookAppointmentThree() {
+		
+	
+        //click Make Appointment
+        driver.findElement(By.xpath("//a[@id='btn-make-appointment']")).click();
+        
+        //Facility
+        //driver.findElement(By.xpath("//select[@id='combo_facility']")).click();
+        //driver.findElement(By.xpath("//option[@value='Hongkong CURA Healthcare Center']")).click();
+        
+        // Select Facility 
+        WebElement selectElementBryanSteph = driver.findElement(By.xpath("//select[@id='combo_facility']")); 
+        Select selectFacilty = new Select(selectElementBryanSteph); 
+        
+        selectFacilty.selectByValue("Seoul CURA Healthcare Center");
+        
+        //readmission
+        driver.findElement(By.xpath("//label[normalize-space()='Apply for hospital readmission']")).click();
+        
+        //healthcare program        
+        driver.findElement(By.xpath("//label[normalize-space()='None']")).click();
         
         
+        //input visitdate
+        driver.findElement(By.xpath("//input[@id=\"txt_visit_date\"]")).sendKeys("25/08/2023");
         
+        //input comment
+        driver.findElement(By.xpath("//textarea[@id=\"txt_comment\"]")).sendKeys("Sample comment only.");
         
-        
-        //driver.close();
-        
-        
+        //click Book Appointment 
+        driver.findElement(By.xpath("//button[@id='btn-book-appointment']")).click();
+         
+   
+	}
+	
+	@AfterTest 
+	public void tearDown() { 
+		driver.quit(); 
+		} 
 	}
 
-}
